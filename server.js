5697c42f9202827354f2894fa108db2be8901510
Haymanot M. <157070689+Haymina@users.noreply.github.com>
2026-03-07 00:12:50 +0300

    Day 3: mongoDB connection with mongoose

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const http = require("http");
+const express = require("express");
+const mongoose = require("mongoose");
 
-const server = http.createServer((req, res) => {
-  res.write("Server is running");
-  res.end();
+const app = express();
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/sundayschoolDB")
+.then(() => {
+    console.log("MongoDB connected successfully");
+})
+.catch((err) => {
+    console.log("MongoDB connection error:", err);
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.get("/", (req, res) => {
+    res.send("Sunday School System API is running");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
 });
