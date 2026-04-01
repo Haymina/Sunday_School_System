3e1e7dbaa23f39777ce2a1c57211a4ab5137b6d7
Haymanot <haymanotmersha.21@gmail.com>
2026-04-01 16:56:32 +0300

    u

--- server.js
+++ server.js
@@ -6,45 +6,13 @@ const adminRoutes = require("./routes/adminRoutes");
 
 connectDB();
 
-const http = require("http");
+const app = express();
+app.use(express.json());
 
-const server = http.createServer((req, res) => {
-  res.write("Server is running");
-  res.end();
+app.use("/members", memberRoutes);
+app.use("/admin", adminRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
-
-
-// Example of inserting one member
-const newMember = new Member({
-    memberId: "M001",
-    fullName: "Abel Tesfaye Yohannes",
-    dateOfBirth: new Date("2010-05-20"),
-    gender: "Male",
-    phoneNumber: "0912345678",
-    momsFullName: "Selamawit Bekele",
-    christianityName: "Abel",
-    emergencyContact: { name: "Dawit", phone: "0911223344" },
-    fatherOfRepentance: { name: "Ephrem", phone: "0911334455" },
-    memberType: "Regular",
-    registrationDate: new Date(),
-    membershipRegistrationDate: new Date(),
-    memberLevel: "Beginner",
-    jobType: "Student",
-    maternityStatus: "N/A",
-    address: "Addis Ababa",
-    photo: "path/to/photo.jpg",
-    status: "Active"
-});
-
-newMember.save()
-    .then(() => console.log("Member saved successfully"))
-    .catch(err => console.log(err));
-
-async function getAllMembers() {
-  const members = await Member.find();
-  console.log(members);
-};
