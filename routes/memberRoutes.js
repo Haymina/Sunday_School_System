9a1e92ef93cf642c2015ff4bdfca1397701c789f
Haymanot <haymanotmersha.21@gmail.com>
2026-04-02 16:58:06 +0300

    commited

--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Member = require('../models/member');
 
 router.get("/", (req, res) => {
     res.send("member route working");
@@ -15,3 +16,30 @@ router.get("/search", async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: "Error searching members", error });
     }
+});
+
+router.get("/status", async (req, res) => {
+    try {
+        const { status } = req.query;
+        const members = await Member.find({ status: status });
+        res.json(members);
+    } catch (error) {
+        res.status(500).json({ message: "Error searching members", error });
+    }
+});
+router.get ("/phone", async ( req, res) => {
+    try {
+        const{phoneNumber} = req.query;
+        const members = await Member.find({ phoneNumber: phoneNumber });
+        res.json(members);
+    } catch (error) {
+        res.status(500).json({ message: "Error searching members by phone number", error });
+    }
+});
+
+/* Next Step (Day 20)
+We will:
+
+Create Admin account
+Test login
+Use Postman / Thunder Client */
